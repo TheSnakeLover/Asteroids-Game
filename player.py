f7e9e6ac9edec76eafed9519045434fb903cd3a5
Ethan Bock <351378@bsd48.org>
2020-08-11 19:00:49 +0000

    Player update

--- player.py
+++ player.py
@@ -1,4 +1,13 @@
-# import packages
+# import package
 import pygame
 
-class Player
+class Player(pygame.sprite.Sprite):
+	def __init__(self, image_name, scale):
+		# get image
+		self.image = pygame.image.load(image_name)
+		# get the rect
+		self.rect = self.image.get_rect()
+		# scale the image
+		self.image = pygame.transform.scale(self.image, (int(scale*self.rect.width), int(scale*self.rect.height)))
+		# scale the rect
+		self.rect = self.image.get_rect()
