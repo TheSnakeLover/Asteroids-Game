79b076b935d9f2383a94429bbc75d55939e50705
Ethan Bock <351378@bsd48.org>
2020-08-11 19:27:36 +0000

    Position

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ background_rect = background_image.get_rect()
 background_rect.center = (width // 2, height // 2)
 
 # set up the player
-player = Player(image_name = "balloon.png", scale = 0.5)
+player = Player(image_name = "balloon.png", scale = 0.5, pos = (50, 100))
 
 def main():
 	while True:
--- player.py
+++ player.py
@@ -2,7 +2,7 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, image_name, scale):
+	def __init__(self, image_name, scale, pos):
 		# get the image
 		self.image = pygame.image.load(image_name)
 		# get the rect
@@ -11,3 +11,8 @@ class Player(pygame.sprite.Sprite):
 		self.image = pygame.transform.scale(self.image, (int(scale*self.rect.width), int(scale*self.rect.height)))
 		# scale the rect
 		self.rect = self.image.get_rect()
+		#setup player's position
+		self.reset(pos)
+
+	def reset(self, pos):
+		self.rect.center = pos
